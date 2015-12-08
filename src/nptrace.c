83ebf8ce64acb241add58181b13d3073b8feffc8
Pål-Erik Martinsen <palerikm@gmail.com>
2015-12-08 10:12:12 +0100

    Moved whois lookout out to new files.

--- src/nptrace.c
+++ src/nptrace.c
@@ -7,7 +7,6 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
-#include <string.h>
 #include <inttypes.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -37,6 +36,7 @@
 #include "utils.h"
 #include "iphelper.h"
 #include "sockethelper.h"
+#include "ip_query.h"
 
 int                        sockfd;
 int                        icmpSocket;
@@ -74,202 +74,6 @@ struct trace_config {
   struct sockaddr_storage localAddr;
 };
 
-int
-whois_query(char*  server,
-            char*  query,
-            char** response)
-{
-  char message[100], buffer[1500];
-  int  read_size, total_size = 0;
-  int error;
-
-  /* char msg[INET6_ADDRSTRLEN + 2]; */
-  int querySocket;
-  /* Test query */
-  struct addrinfo hints, * res;
-
-  (void) server;
-  /* first, load up address structs with getaddrinfo(): */
-
-  memset(&hints, 0, sizeof hints);
-  hints.ai_family   = AF_UNSPEC;
-  hints.ai_socktype = SOCK_STREAM;
-
-  /* getaddrinfo("freegeoip.net", "80", &hints, &res); */
-  /* getaddrinfo("whois.iana.org", "43", &hints, &res); */
-  error = getaddrinfo(server, "43", &hints, &res);
-  if( error){
-    return error;
-  }
-  /* make a socket: */
-  querySocket = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-
-  /* connect! */
-  if (connect(querySocket, res->ai_addr, res->ai_addrlen) < 0)
-  {
-    perror("connect failed when trying to do whois query");
-  }
-
-
-
-  /* Now send some data or message */
-  //("\nQuerying for ... %s ... (%s)", query, server);
-  sprintf(message, "%s\r\n", query);
-  if (send(querySocket, message, strlen(message), 0) < 0)
-  {
-    perror("send failed");
-  }
-
-  /* Now receive the response */
-  while ( ( read_size = recv(querySocket, buffer, sizeof(buffer), 0) ) )
-  {
-    *response = realloc(*response, read_size + total_size);
-    if (*response == NULL)
-    {
-      printf("realloc failed");
-    }
-    memcpy(*response + total_size, buffer, read_size);
-    total_size += read_size;
-  }
-  //printf("Done");
-  fflush(stdout);
-
-  *response                 = realloc(*response, total_size + 1);
-  *(*response + total_size) = '\0';
-
-  close(querySocket);
-  return 0;
-}
-
-
-int
-parseOrigin(char* as)
-{
-  int num = 0;
-  if (as != NULL)
-  {
-    char* cnum;
-
-    cnum = strrchr(as, ':');
-    cnum++;
-    while ( isspace(*cnum) )
-    {
-      cnum++;
-    }
-    num = atoi(cnum + 2);
-    return num;
-  }
-  return 0;
-}
-
-int
-getAsNumber(char* buffer,
-            char* nextServer,
-            int   sizeSrv)
-{
-  char* tok, * as, * serv;
-  int   num;
-
-  tok = strtok(buffer, "\n");
-  while (tok != NULL)
-  {
-    /* Check for origin: */
-    as = strstr(tok, "origin:");
-    if (as != NULL)
-    {
-      num = parseOrigin(tok);
-      if (num != 0)
-      {
-        return num;
-      }
-    }
-    as = strstr(tok, "OriginAS:");
-    if (as != NULL)
-    {
-      num = parseOrigin(tok);
-      if (num != 0)
-      {
-        return num;
-      }
-    }
-
-    /* While we are at it set nextserv as well */
-    serv = strstr(tok, "refer:");
-    if ( (serv != NULL) )
-    {
-      serv = serv + 7;
-      while ( isspace(*serv) )
-      {
-        serv++;
-      }
-      strncpy(nextServer, serv, sizeSrv);
-    }
-    serv = strstr(tok, "ReferralServer:");
-    if ( (serv != NULL) )
-    {
-      serv = serv + 16;
-      while ( isspace(*serv) )
-      {
-        serv++;
-      }
-      serv = serv + strlen("whois://");
-      //printf("Setting next server!\n");
-      strncpy(nextServer, serv, sizeSrv);
-    }
-    /* Next line */
-    tok = strtok(NULL, "\n");
-  }
-    return 0;
-}
-
-
-int
-asLookup(char* ip)
-{
-  char* response = NULL;
-
-  int  asNum = 0;
-  char server[256];
-  /* char server[255]; */
-
-  if ( whois_query("whois.iana.org", ip, &response) )
-  {
-        printf("Whois query failed");
-  }
-  asNum = getAsNumber( response, server, sizeof(server) );
-
-
-  if (asNum > 0)
-  {
-        //printf("AS: %i\n",              asNum);
-        return asNum;
-  }
-  else
-  {
-    if ( whois_query(server, ip, &response) )
-    {
-        //printf("Whois query failed");
-        return 0;
-    }
-        //printf("%s", response);
-    asNum = getAsNumber( response, server, sizeof(server) );
-    if (asNum <= 0)
-    {
-        //printf("Still no AS num (%s)\n\n\n", server);
-      if ( whois_query(server, ip, &response) )
-      {
-        printf("Whois query failed");
-        return 0;
-      }
-        //printf("%s",                         response);
-        asNum = getAsNumber( response, server, sizeof(server) );
-        //printf("AS NUM: %i",                 asNum);
-    }
-  }
-  free(response);
-
-  return asNum;
-}
 
 void
 printTimeSpent(uint32_t wait)
